4449f20731f339919a09aa64d67b981832a4e3bb
CardoPixel <cardo.pixelado@gmail.com>
2025-05-01 18:07:44 -0300

    ci(app): :construction_worker: Added automatic release GitHub Action
    - Clean up from Create Next.js App files. - Added release-it, configurations and dependencies on package.json and .husky/commit-msg. - Restructured directory structure to prepare for shadcn/ui components. - Added a backup of the original file that inspire the looks of this project. - Replaced next.config.ts for next.config.mjs. - Updated .gitignore file. - Updated tsconfig,json file.
    
    No breaking changes

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  },
+  experimental: {
+  },
+  serverExternalPackages: [],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
